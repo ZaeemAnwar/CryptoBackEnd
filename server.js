008d3152d96291c4d7083453feb69b79ec1303db
ZaeemAnwar <zaeemanwar@gmail.com>
2024-01-14 18:22:44 -0500

    WIP: Get Logos metadata

--- server.js
+++ server.js
@@ -33,4 +33,27 @@ app.get("/api/crypto", (req, res)=>{
             });
 })
 
+// WIP: Used to get the logos for the crypto companies (as well as any other meta data)
+app.get("/api/crypto/logos", (req, res)=>{
+  console.log("Recieved request to get updated crypto data")
+  console.log ("request shape is : " , JSON.stringify(req.query))
+  const ids = req.query.id.split(',').map(id => parseInt(id, 10));
+
+  Axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id='+ids, {
+        headers: {
+          'Accepts': 'application/json',
+          'Accept-Encoding': 'deflate, gzip',
+          'X-CMC_PRO_API_KEY': process.env.CRYPTO_API_KEY,
+        },
+      }).then(response => {
+          console.log("GET LOGOS was successful, attempting to return to client " + JSON.stringify(response.data))
+          res.send(response.data)
+      })
+      .catch(err => {
+          console.log("GET LOGOS Ran into issue")
+          console.log("Issue details: ", err)
+          });
+})
+
+
 app.listen(process.env.PORT, ()=>{console.log("Server started on port " + process.env.PORT)})
